568e4870c8b2db3e827f10ff169e42c45df7ee78
fusion809 <brentonhorne77@gmail.com>
2016-01-18 04:48:16 +1000

    Updating scripts

---
+++ bulk-rename.py
@@ -0,0 +1,7 @@
+import glob, os
+
+def rename(dir, pattern, titlePattern):
+    for pathAndFilename in glob.iglob(os.path.join(dir, pattern)):
+        title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+        os.rename(pathAndFilename,
+                  os.path.join(dir, titlePattern % title + ext))
--- simpenscisol.py
+++ simpenscisol.py
@@ -5,27 +5,32 @@ import numpy as np
 from scipy import integrate
 from math import cos
 from matplotlib import pyplot as plt
-a, b = 0, 10
+a, b = 0, 5
 g, l = 9.8, 1
-N = 10000000
+N = 1000000000
 h = (b-a) / float(N)
 
 def f(x, t):
     return [x[1], -g/l*cos(x[0])]
 
 t    = np.arange(a, b, h)
-sol  = integrate.odeint(f, [0, 0], t)
+sol  = integrate.odeint(f, [0, 0], t, rtol=1e-15)
 th   = sol[:,0]
 dth  = sol[:,1]
-
+Err=[abs(min(th)+np.pi),max(th)]
+print(max(Err))
+print("The minimum value of dth is",min(dth))
+print("The maximum value of dth is",max(dth))
+print("The minimum value of th is",min(th))
+print("The maximum value of th is",max(th))
 # Call the plot function if you want to plot theta against t
 # and a phase plot of d{theta}/dt against theta.
 def plot():
     plt.figure(1)
-    plt.plot(t,th)
+    plt.plot(t,sol)
 
     plt.figure(2)
     plt.plot(th,dth)
     plt.show()
 
-plot()
+#plot()
