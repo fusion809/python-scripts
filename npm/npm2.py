8dae8eff071625fba8dbf79491d3f3e93ce95d47
fusion809 <brentonhorne77@gmail.com>
2016-04-06 10:26:43 +1000

    Updating

--- npm/npm2.py
+++ npm/npm2.py
@@ -2,11 +2,24 @@ import json
 from sys import argv
 print(argv[1])
 from subprocess import call
+from subprocess import Popen, PIPE
 
-for i in argv[1]:
-    with open("/home/fusion809/OBS/home:fusion809:arch_extra/nodejs-" + argv[1] + "/src/package/package.json") as json_file:
-        json_data = json.load(json_file)
-        deps = json_data["dependencies"]
-        for key, value in deps.items():
-            print(key)
-            call(["cpobsn", key])
+with open("/home/fusion809/OBS/home:fusion809:arch_extra/nodejs-" + argv[1] + "/src/package/package.json") as json_file:
+    json_data = json.load(json_file)
+    deps = json_data["dependencies"]
+    LEN=len(deps)
+    print(LEN)
+    i=0
+    DEP=list()
+    print(DEP)
+    for key, value in deps.items():
+        print(key)
+        DEP.append(key)
+        i = i+1
+        print(i)
+        #call(["cpobsn", key, argv[1]])
+    p=Popen(["depends"] + DEP, stdout=PIPE)
+    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+    rc = p.returncode
+    print(output)
+    call(["moddepends", "nodejs-" + argv[1], output])
