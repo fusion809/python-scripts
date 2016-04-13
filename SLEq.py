dcadf4c1ab2cf73eef8cf122089efc401e913b41
fusion809 <brentonhorne77@gmail.com>
2016-04-13 20:43:22 +1000

    Updating

--- SLEq.py
+++ SLEq.py
@@ -3,7 +3,7 @@ from scipy import linalg as LA
 
 N=100
 n=np.arange(0,N+1)
-nsub=np.arange(0,N-1)
+nsub=np.arange(1,N)
 n=np.asmatrix(n)
 x=np.cos(np.pi*n/N)
 x=x.T
@@ -16,9 +16,19 @@ for i in na:
     Un[i,i] = i
 
 for i in nsub:
-    Ux[i,i] = np.divide(1,np.sqrt(1-np.power(x[i+1],2)))
+    print(i)
+    Ux[i-1,i-1] = np.divide(1,np.sqrt(1-np.power(np.cos(np.pi*i/N),2)))
 
-Usub=Ux*np.sin(np.arccos(xsub)*n)*Un
-Um1=np.multiply(np.power(-1,n+1),np.power(n,2))
-Up1=np.power(n,2)
-U=[Um1, Usub, Up1]
+dTsub=Ux*np.sin(np.arccos(xsub)*n)*Un
+dTm1=np.multiply(np.power(-1,n+1),np.power(n,2))
+dTp1=np.power(n,2)
+for i in n:
+    for j in n:
+        if i == 0:
+            dT[i,j] = dTm1[j]
+        elif i == N:
+            dT[i,j] = dTp1[j]
+        else:
+            dT[i,j]=dTsub[i-1,j]
+
+D1=LA.inv(dT)
