0e346e81122c0335e08b40851b8349a26c0a0634
fusion809 <brentonhorne77@gmail.com>
2016-08-30 19:49:44 +1000

    Adding simpleq-2nd-RK4.py

---
+++ simpleq-2nd-RK4.py
@@ -0,0 +1,38 @@
+from numpy import cos
+from numpy import matrix
+
+def f(x, y, dy):
+    return - 9.8 * cos(y)
+
+def RK4(h, x, y, dy):
+    K1=h * f(x, y, dy)
+    L1=h * dy
+    K2=h * f(x + h/2, y + L1/2, dy + K1/2)
+    L2=h * (dy + K1/2)
+    K3=h * f(x + h/2, y + L2/2, dy + K2/2)
+    L3=h * (dy + K2/2)
+    K4=h * f(x + h, y + L3, dy + K3)
+    L4=h * (dy + K3)
+    return matrix([1/6*(L1+2*L2+2*L3+L4), 1/6*(K1+2*K2+2*K3+K4)])
+
+x0=0
+x1=10
+rtol=1e-4
+y0=0
+dy0=0
+N=1000
+h=(x1-x0)/N
+x=x0
+y=y0
+dy=dy0
+
+while x < x1 - rtol:
+    RK = RK4(h, x, y, dy)
+    y = y + RK[0,0]
+    dy = dy + RK[0,1]
+    x = x + h
+
+print(RK[0,0])
+print(RK[0,1])
+print("x is", x)
+print("y is", y)
