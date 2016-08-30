25356ffe0578bec13ec2f1dce6199f56f5b7ba16
fusion809 <brentonhorne77@gmail.com>
2016-08-30 18:46:32 +1000

    Updating

--- simpleq-RK4.py
+++ simpleq-RK4.py
@@ -1,13 +1,13 @@
 from numpy import cos
 
-def f(x,y):
+def f(x, y):
     return x * cos(y)
 
-def RK4(h,x,y):
+def RK4(h, x, y):
     K1=h*f(x, y)
     K2=h*f(x+h/2, y+K1/2)
     K3=h*f(x+h/2, y+K2/2)
-    K4=h * f(x+h, y+K3)
+    K4=h*f(x+h, y+K3)
     return 1/6*(K1+2*K2+2*K3+K4)
 
 x0=0
