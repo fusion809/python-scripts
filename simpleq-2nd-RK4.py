e0bf54b123d2c6534c4afdf813166907075f8b5e
fusion809 <brentonhorne77@gmail.com>
2016-08-31 20:03:05 +1000

    [simpleq-2nd-RK4.py] Updating

--- simpleq-2nd-RK4.py
+++ simpleq-2nd-RK4.py
@@ -4,9 +4,12 @@ from numpy import cos, matrix, pi
 # The ODE we're solving is (note d2 = d^2 in the following equations):
 # d2y/dx2 = -g/l cos(y)
 # (which is the equation of the Simple Pendulum where y is the angle
-# from the x axis; and x is the time in seconds)
+# from the x axis; and x is the time in seconds,
 # g is the acceleration due to gravity, namely ~9.8 m/s2
-# l is the length of the rod
+# l is the length of the rod)
+# this equation can be re-written as:
+# d2y/dx2 = f(x, y, dy)
+# where f(x, y, dy) is the function defined below
 def f(x, y, dy):
     g = 9.8
     l = 1
