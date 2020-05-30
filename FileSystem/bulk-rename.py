efebe4aea161a6eaed8d4d9fd926c850c72e3c4b
Brenton Horne <brentonhorne77@gmail.com>
2020-05-31 03:34:23 +1000

    Rm FS folder

--- FileSystem/bulk-rename.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# URL: http://stackoverflow.com/a/227125
-import glob, os
-
-def rename(dir, pattern, titlePattern):
-    for pathAndFilename in glob.iglob(os.path.join(dir, pattern)):
-        title, ext = os.path.splitext(os.path.basename(pathAndFilename))
-        os.rename(pathAndFilename,
-                  os.path.join(dir, titlePattern % title + ext))
