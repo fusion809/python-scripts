d962e411d0174a6852aaa4e69840f7c36ad88b92
Brenton Horne <brentonhorne77@gmail.com>
2020-08-07 08:38:17 +1000

    Fixing RK45 3d solver

--- IVP/RK45_3d.py
+++ IVP/RK45_3d.py
@@ -3,6 +3,7 @@
 # Freezes up with high CPU usage
 from numpy import *
 from matplotlib import pyplot as plt
+import matplotlib
 
 # dr/dt = f(t,x,y,z)
 def f(t,x,y,z):
@@ -40,38 +41,42 @@ def RK45(f, t0, tf, dtInitial, x0, y0, z0, epsilon):
         dt = min([dt, tf-t[i]])
         
         # r approtimators
-        K1 = multiply(dt,f(t[i],x[i],y[i],z[i]))
+        K1 = multiply(dt,f(t[i], x[i], y[i], z[i]))
         k1 = K1[0]
         l1 = K1[1]
         m1 = K1[2]
-        K2 = multiply(dt,f(t[i]+dt/4,x[i]+k1/4,y[i]+l1/4,z[i]+m1/4))
+
+        K2 = multiply(dt,f(t[i]+dt/4, x[i]+k1/4, y[i]+l1/4, z[i]+m1/4))
         k2 = K2[0]
         l2 = K2[1]
         m2 = K2[2]
+
         K3 = multiply(dt,f(t[i]+3*dt/8, x[i]+3*k1/32+9*k2/32, y[i]+3*l1/32+9*l2/32, z[i]+3*m1/32+9*m2/32))
         k3 = K3[0]
         l3 = K3[1]
         m3 = K3[2]
+
         K4 = multiply(dt,f(t[i]+12*dt/13, x[i]+1932*k1/2197-7200*k2/2197+7296*k3/2197, y[i]+1932*l1/2197-7200*l2/2197+7296*l3/2197, z[i]+1932*m1/2197-7200*m2/2197+7296*m3/2197))
         k4 = K4[0]
         l4 = K4[1]
         m4 = K4[2]
-        K5 = multiply(dt,f(t[i]+dt, x[i]+439*k1/216 - 8*k2 + 3680*k3/513 - 845*k4/4104, y[i]+439*l1/216 - 8*l2 + 3680*l3/513 - 845*l4/4104, z[i]+439*m1/216 - 8*l2 + 3680*m3/513 - 845*m4/4104))
+
+        K5 = multiply(dt,f(t[i]+dt, x[i]+439*k1/216-8*k2+3680*k3/513-845*k4/4104, y[i]+439*l1/216-8*l2+3680*l3/513-845*l4/4104, z[i]+439*m1/216-8*m2+3680*m3/513-845*m4/4104))
         k5 = K5[0]
         l5 = K5[1]
         m5 = K5[2]
+
         K6 = multiply(dt,f(t[i]+dt/2, x[i]-8*k1/27+2*k2-3544*k3/2565+1859*k4/4104-11*k5/40, y[i]-8*l1/27+2*l2-3544*l3/2565+1859*l4/4104-11*l5/40, z[i]-8*m1/27+2*m2-3544*m3/2565+1859*m4/4104-11*m5/40))
         k6 = K6[0]
         l6 = K6[1]
         m6 = K6[2]
 
-        # r[i+1] approtimations
-        x1 = x[i] + 25*k1/216 + 1408*k3/2565+2197*k4/4104-k5/5
-        x2 = x[i] + 16*k1/135 + 6656*k3/12825 + 28561*k4/56430 - 9*k5/50 + 2*k6/55
-        y1 = y[i] + 25*l1/216 + 1408*l3/2565+2197*l4/4104-l5/5
-        y2 = y[i] + 16*l1/135 + 6656*l3/12825 + 28561*l4/56430 - 9*l5/50 + 2*l6/55
-        z1 = z[i] + 25*m1/216 + 1408*m3/2565+2197*m4/4104-m5/5
-        z2 = z[i] + 16*m1/135 + 6656*m3/12825 + 28561*m4/56430 - 9*m5/50 + 2*m6/55
+        x1 = x[i] + 25*k1/216+1408*k3/2565+2197*k4/4104-k5/5
+        y1 = y[i] + 25*l1/216+1408*l3/2565+2197*l4/4104-l5/5
+        z1 = z[i] + 25*m1/216+1408*m3/2565+2197*m4/4104-m5/5
+        x2 = x[i] + 16*k1/135+6656*k3/12825+28561*k4/56430-9*k5/50+2*k6/55
+        y2 = y[i] + 16*l1/135+6656*l3/12825+28561*l4/56430-9*l5/50+2*l6/55
+        z2 = z[i] + 16*m1/135+6656*m3/12825+28561*m4/56430-9*m5/50+2*m6/55
 
         # A representation of the error in our x approtimation
         Rx = abs(x1-x2)/dt
@@ -86,7 +91,7 @@ def RK45(f, t0, tf, dtInitial, x0, y0, z0, epsilon):
 
         # If R is less than or equal to epsilon, move onto the nett step, 
         # otherwise repeat the iteration with our corrected dt value
-        if ( Rx <= epsilon) & (Ry <= epsilon ):
+        if ( Rx <= epsilon) & (Ry <= epsilon) & (Rz <= epsilon):
             t = append(t,[t[i]+dt], axis=0)
             x = append(x,[x1], axis=0)
             y = append(y,[y1], axis=0)
@@ -103,7 +108,7 @@ def RK45(f, t0, tf, dtInitial, x0, y0, z0, epsilon):
 t0 = 0.0
 tf = 100
 # Error tolerance
-epsilon = 1e-3
+epsilon = 1e-8
 # Initial condition
 x0 = 10
 y0 = 10
@@ -111,7 +116,7 @@ z0 = 10
 [t,x,y,z] = RK45(f, t0, tf, (tf-t0)/1000, x0, y0, z0, epsilon)
 
 # Plot our solution
-plt.rcParams["figure.figsixe"] = (25,14)
+matplotlib.use('WXAgg')
 plt.figure(1)
 plt.plot(t,x)
 plt.figure(2)
