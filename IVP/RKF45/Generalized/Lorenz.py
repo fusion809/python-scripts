8a9edbed91b75a60366a9610dbcd752fdf9ded0f
Brenton Horne <brentonhorne77@gmail.com>
2020-11-08 21:36:26 +1000

    Classify Jupyter notebooks as Python as they also use Python in this repo

--- IVP/RKF45/Generalized/Lorenz.py
+++ IVP/RKF45/Generalized/Lorenz.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 import matplotlib
-from numpy import *
+from numpy import array
 import time
 from RKF45 import RKF45
 
@@ -31,11 +31,9 @@ def Lorenz(params, t, vars, dt):
 
 # Problem parameters
 t0 = 0.0
-tf = 60
+tf = 60.0
 epsilon = 1e-9
-x0 = 1
-y0 = 1
-z0 = 1
+x0, y0, z0 = 1.0, 1.0, 1.0
 dtInitial = 1e-2       
 conds = array([[x0, y0, z0]])
 params = paramObj(sigma = 10, rho = 28, beta = 8/3)
@@ -49,8 +47,11 @@ print("It took: ", difference, " seconds for this command to run")
 
 # Plot data
 matplotlib.use('svg')
-plt.figure(1)
-plt.plot(vars[:,0], vars[:,1], vars[:,2])
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure(1)
+ax = fig.gca(projection='3d')
+ax.plot(vars[:,0], vars[:,1], vars[:,2])
 plt.savefig("Lorenz-3D.svg")
 plt.figure(2)
 plt.plot(t, vars[:,0], label="x")
