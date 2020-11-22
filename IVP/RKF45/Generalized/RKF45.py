25026a8172d3af5b24f1c9c0c42cd7a5d407f5ab
Brenton Horne <brentonhorne77@gmail.com>
2020-11-22 12:21:13 +1000

    Adhering to 80 char limit

--- IVP/RKF45/Generalized/RKF45.py
+++ IVP/RKF45/Generalized/RKF45.py
@@ -24,37 +24,44 @@ def RKF45(f, t0, tf, dtInitial, params, conds, epsilon):
     """
     i = 0
     t = array([t0])
-    vars = conds
+    depVars = conds
     dt = dtInitial
 
     # Loop over time values
     while (t[i] < tf):
         dt = min([dt, tf-t[i]])
 
-        # Approximators of vars[i+1, :]
-        K1 = f(params, t[i], vars[i,:], dt)
-        K2 = f(params, t[i] + dt/4, vars[i,:] + multiply(1/4, K1), dt)
-        K3 = f(params, t[i] + 3*dt/8, vars[i,:] + multiply(3/32, K1) + multiply(9/32, K2), dt)
-        K4 = f(params, t[i] + 12*dt/13, vars[i,:] + multiply(1932/2197, K1) + multiply(-7200/2197, K2) + multiply(7296/2197, K3), dt)
-        K5 = f(params, t[i] + dt, vars[i,:] + multiply(439/216, K1) + multiply(-8, K2) + multiply(3680/513, K3) + multiply(-845/4104, K4), dt)
-        K6 = f(params, t[i] + dt/2, vars[i,:] + multiply(-8/27, K1) + multiply(2, K2) + multiply(-3544/2565, K3) + multiply(1859/4104, K4) + multiply(-11/40, K5), dt)
+        # Approximators of depVars[i+1, :]
+        K1 = f(params, t[i], depVars[i,:], dt)
+        K2 = f(params, t[i] + dt/4, depVars[i,:] + multiply(1/4, K1), dt)
+        K3 = f(params, t[i] + 3*dt/8, depVars[i,:] + multiply(3/32, K1) + 
+        multiply(9/32, K2), dt)
+        K4 = f(params, t[i] + 12*dt/13, depVars[i,:] + multiply(1932/2197, K1) + 
+        multiply(-7200/2197, K2) + multiply(7296/2197, K3), dt)
+        K5 = f(params, t[i] + dt, depVars[i,:] + multiply(439/216, K1) + 
+        multiply(-8, K2) + multiply(3680/513, K3) + multiply(-845/4104, K4), dt)
+        K6 = f(params, t[i] + dt/2, depVars[i,:] + multiply(-8/27, K1) + 
+        multiply(2, K2) + multiply(-3544/2565, K3) + multiply(1859/4104, K4) + 
+        multiply(-11/40, K5), dt)
 
-        # 4th order approx for vars[i+1,:]
-        vars1 = vars[i,:] + multiply(25/216, K1) + multiply(1408/2565, K3) + multiply(2197/4104, K4) + multiply(-1/5, K5)
-        # 5th order approx for vars[i+1,:]
-        vars2 = vars[i,:] + multiply(16/135, K1) + multiply(6656/12825, K3) + multiply(28561/56430, K4) + multiply(-9/50, K5) + multiply(2/55, K6)
+        # 4th order approx for depVars[i+1,:]
+        depVars1 = depVars[i,:] + multiply(25/216, K1) + multiply(1408/2565, K3) 
+        + multiply(2197/4104, K4) + multiply(-1/5, K5)
+        # 5th order approx for depVars[i+1,:]
+        depVars2 = depVars[i,:] + multiply(16/135, K1) + multiply(6656/12825, K3) 
+        + multiply(28561/56430, K4) + multiply(-9/50, K5) + multiply(2/55, K6)
         
-        # Rarr is an array of error estimates for vars[i+1,:]
-        Rarr = abs((vars2-vars1)/dt)
+        # Rarr is an array of error estimates for depVars[i+1,:]
+        Rarr = abs((depVars2-depVars1)/dt)
         R = max(Rarr)
         
         # Correct step size, if error estimate is too big
         s = (epsilon/(2*R))**(1/4)
-        if (R <= epsilon):
+        if R <= epsilon:
             t = append(t, [t[i]+dt], axis=0)
-            vars = append(vars, [vars1], axis=0)
+            depVars = append(depVars, [depVars1], axis=0)
             i += 1
         
         dt *= s
 
-    return t, vars
+    return t, depVars
