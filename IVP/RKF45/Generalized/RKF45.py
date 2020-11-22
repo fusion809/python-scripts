5c7697bc2294bbfb90840e1721762e28d2c1e6fc
Brenton Horne <brentonhorne77@gmail.com>
2020-11-22 12:53:04 +1000

    80 char limit can't seem to be kept to in Python

--- IVP/RKF45/Generalized/RKF45.py
+++ IVP/RKF45/Generalized/RKF45.py
@@ -34,22 +34,18 @@ def RKF45(f, t0, tf, dtInitial, params, conds, epsilon):
         # Approximators of depVars[i+1, :]
         K1 = f(params, t[i], depVars[i,:], dt)
         K2 = f(params, t[i] + dt/4, depVars[i,:] + multiply(1/4, K1), dt)
-        K3 = f(params, t[i] + 3*dt/8, depVars[i,:] + multiply(3/32, K1) + 
-        multiply(9/32, K2), dt)
-        K4 = f(params, t[i] + 12*dt/13, depVars[i,:] + multiply(1932/2197, K1) + 
-        multiply(-7200/2197, K2) + multiply(7296/2197, K3), dt)
-        K5 = f(params, t[i] + dt, depVars[i,:] + multiply(439/216, K1) + 
-        multiply(-8, K2) + multiply(3680/513, K3) + multiply(-845/4104, K4), dt)
-        K6 = f(params, t[i] + dt/2, depVars[i,:] + multiply(-8/27, K1) + 
-        multiply(2, K2) + multiply(-3544/2565, K3) + multiply(1859/4104, K4) + 
-        multiply(-11/40, K5), dt)
+        K3 = f(params, t[i] + 3*dt/8, depVars[i,:] + multiply(3/32, K1) + multiply(9/32, K2), dt)
+        K4 = f(params, t[i] + 12*dt/13, depVars[i,:] + multiply(1932/2197, K1) + multiply(-7200/2197, K2) + multiply(7296/2197, K3), dt)
+        K5 = f(params, t[i] + dt, depVars[i,:] + multiply(439/216, K1) + multiply(-8, K2) + multiply(3680/513, K3) + multiply(-845/4104, K4), dt)
+        K6 = f(params, t[i] + dt/2, depVars[i,:] + multiply(-8/27, K1) + multiply(2, K2) + multiply(-3544/2565, K3) + multiply(1859/4104, K4) + multiply(-11/40, K5), dt)
 
         # 4th order approx for depVars[i+1,:]
         depVars1 = depVars[i,:] + multiply(25/216, K1) + multiply(1408/2565, K3) 
-        + multiply(2197/4104, K4) + multiply(-1/5, K5)
+        depVars1 += multiply(2197/4104, K4) + multiply(-1/5, K5)
         # 5th order approx for depVars[i+1,:]
         depVars2 = depVars[i,:] + multiply(16/135, K1) + multiply(6656/12825, K3) 
-        + multiply(28561/56430, K4) + multiply(-9/50, K5) + multiply(2/55, K6)
+        depVars2 += multiply(28561/56430, K4) + multiply(-9/50, K5);
+        depVars2 += multiply(2/55, K6)
         
         # Rarr is an array of error estimates for depVars[i+1,:]
         Rarr = abs((depVars2-depVars1)/dt)
