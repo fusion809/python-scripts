380c87ffed717de6472a96641268cd6115aaf405
Brenton Horne <brentonhorne77@gmail.com>
2021-11-09 01:31:48 +1000

    Modularizing simplePendulum.py

--- IVP/RKF45/Generalized/simplePendulum.py
+++ IVP/RKF45/Generalized/simplePendulum.py
@@ -15,27 +15,31 @@ def simpPen(params, t, vars, dt):
     thetaDDot = (-g/l)*np.cos(theta)
     return [dt * thetaDot, dt * thetaDDot]
 
-# Problem parameters
-t0 = 0.0
-tf = 10
-epsilon = 1e-9
-theta0, thetaDot0 = 0, 0
-dtInitial = 1e-2       
-conds = np.array([[theta0, thetaDot0]])    
-params = paramObj(g=9.81, l=1.0)
-t, vars = RKF45(simpPen, t0, tf, dtInitial, params, conds, epsilon)
+def main():
+    # Problem parameters
+    t0 = 0.0
+    tf = 10
+    epsilon = 1e-9
+    theta0, thetaDot0 = 0, 0
+    dtInitial = 1e-2       
+    conds = np.array([[theta0, thetaDot0]])    
+    params = paramObj(g=9.81, l=1.0)
+    t, vars = RKF45(simpPen, t0, tf, dtInitial, params, conds, epsilon)
 
-# Plot data
-matplotlib.use('TkAgg')
-plt.figure(1)
-plt.plot(vars[:,0], vars[:,1])
-plt.xlabel(r"$\theta$")
-plt.ylabel(r"$\dot{\theta}$")
-#plt.savefig("simplePendulum-phasePlot.svg")
-plt.show()
-plt.figure(2)
-plt.plot(t, vars[:,0], label=r"$\theta$")
-plt.plot(t, vars[:,1], label=r"$\dot{\theta}$")
-plt.legend()
-#plt.savefig("simplePendulum-timePlot.svg")
-plt.show()
+    # Plot data
+    matplotlib.use('TkAgg')
+    plt.figure(1)
+    plt.plot(vars[:,0], vars[:,1])
+    plt.xlabel(r"$\theta$")
+    plt.ylabel(r"$\dot{\theta}$")
+    #plt.savefig("simplePendulum-phasePlot.svg")
+    plt.show()
+    plt.figure(2)
+    plt.plot(t, vars[:,0], label=r"$\theta$")
+    plt.plot(t, vars[:,1], label=r"$\dot{\theta}$")
+    plt.legend()
+    #plt.savefig("simplePendulum-timePlot.svg")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
